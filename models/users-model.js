0f58ffc03fff83b8cce1d9305282a04086a3fa7d
Nigel Mukandi <33910227+Chitova263@users.noreply.github.com>
2018-04-28 14:38:44 +0200

    local login completed

--- models/users-model.js
+++ models/users-model.js
@@ -1,8 +1,11 @@
+
 'use strict'
 
 // Imports
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const keys = require('../config/keys')
 
 // UserSchema
 const UserSchema = new mongoose.Schema({
@@ -19,6 +22,31 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+// Presave 
+UserSchema.pre('save', async function(next) {
+    try {
+      // Generate a salt
+      const salt = await bcrypt.genSalt(10);
+      // Generate a password hash (salt + hash)
+      const passwordHash = await bcrypt.hash(this.password, salt);
+      // Re-assign hashed version over original, plain text password
+      this.password = passwordHash;
+      console.log(salt);
+      console.log(passwordHash);
+      next();
+    } catch(error) {
+      next(error);
+    }
+});
+  
+UserSchema.methods.isValidPassword = async function(newPassword) {
+    try {
+      return await bcrypt.compare(newPassword, this.password);
+    } catch(error) {
+      throw new Error(error);
+    }
+}
+  
 // Model
 const User = mongoose.model('user', UserSchema);
 
