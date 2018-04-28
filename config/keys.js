61a4ad6062575da946ee0d53481275fd128db76d
Nigel Mukandi <33910227+Chitova263@users.noreply.github.com>
2018-04-28 03:09:15 +0200

    Add files via upload
    Added JWT tokens module

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,8 @@ module.exports = {
     
     mongodb : {
         dbURI : 'mongodb://nigelmukandi:sergio@ds259499.mlab.com:59499/shopify-net-demo'
-    }
+    },
+
+    JWT_SECRET  : 'unopenga',
 }
+
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,12 +1,29 @@
 'use strict'
 
 // Imports
-const User = require('../models/users-model');
+const JWT = require('jsonwebtoken');
 
+const User = require('../models/users-model');
+const keys = require('../config/keys');
+
+
+/**
+ * Generates Token
+ * @param {*} user
+ */
+const signToken = user => {
+        return  JWT.sign({
+        iss : 'Shopify',                                        // Issuer of token
+        sub :  user._id,                                     // Subject
+        iat :  new Date().getTime(),                            // Issued Time
+        exp : new Date().setDate(new Date().getDate() + 1)      // Token Expiry date
+    }, keys.JWT_SECRET);
+}
 
 // Code to handle routes
 module.exports ={
 
+    //Sign Up
     signUp: async (req, res, next) => {
         console.log('signUp() reached');
         // Password and Email sent with request
@@ -23,11 +40,17 @@ module.exports ={
             email : email,
             password : password
         });
-        //Save NewUser to Database
+        //Save NewUser to Database and Respond With Token
         await newUser.save();
-        res.json({user : 'user created'})
+        //res.json({user : 'user created'});
+        //Generate Token
+        const token = signToken(newUser);
+       
+        //respond with token
+        res.status(200).json({token : token});
     },
 
+    //SignIn
     signIn: async (req, res, next)=>{
         console.log('signIn() reached');
     },
